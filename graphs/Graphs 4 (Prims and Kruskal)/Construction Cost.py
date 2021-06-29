0495191848717a39fac5e94141148a53cf5545d3
Paarth Vashisht <paarth2321@gmail.com>
2021-06-29 22:01:00 +0530

    Added graph 4

---
+++ graphs/Graphs 4 (Prims and Kruskal)/Construction Cost.py
@@ -0,0 +1,38 @@
+import heapq as hp
+mod=10**9+7
+
+def return_adj_lis(nodes_available, connected_nodes):
+    adj_arr = [[]for j in range(nodes_available+1)]
+    for i in connected_nodes:
+        if i[0] != i[1]:
+            adj_arr[i[0]].append([i[2], i[1]])
+            adj_arr[i[1]].append([i[2], i[0]])
+    return adj_arr
+
+
+def return_min_cost_of_MST(adj_arr):
+    traversed_nodes = {1}
+    cost = 0
+    min_hp = []
+    hp.heapify(min_hp)
+    for i in adj_arr[1]:
+        hp.heappush(min_hp, i)
+    while min_hp:
+        result = hp.heappop(min_hp)
+        if result[1] not in traversed_nodes:
+            traversed_nodes.add(result[1])
+            cost += result[0]
+            cost%=mod
+            for i in adj_arr[result[1]]:
+                if i[1] not in traversed_nodes:
+                    hp.heappush(min_hp, i)
+    return cost
+
+
+class Solution:
+    # @param A : integer
+    # @param B : list of list of integers
+    # @return an integer
+    def solve(self, A, B):
+        adj_arr = return_adj_lis(A, B)
+        return return_min_cost_of_MST(adj_arr)
